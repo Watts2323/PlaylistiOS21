342fb15f2a8400d97d79f201daf9b496f393c01f
Xavier <xwatts2323@Xaviers-MacBook-Pro.local>
2018-08-29 11:11:10 -0600

    Added models, updated or UI's and started coding

---
+++ Models/Playlist.swift
@@ -0,0 +1,43 @@
+//
+//  Playlist.swift
+//  PlaylistiOSP2
+//
+//  Created by Xavier on 8/29/18.
+//  Copyright © 2018 Xavier ios dev. All rights reserved.
+//
+
+import Foundation
+
+class Playlist {
+    var title: String
+    var songs: [Song] = []
+    
+    init(title: String, song: [Song] = []) {
+        self.title = title
+        self.songs = songs
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Models/Song.swift
@@ -0,0 +1,29 @@
+//
+//  Song.swift
+//  PlaylistiOSP2
+//
+//  Created by Xavier on 8/29/18.
+//  Copyright © 2018 Xavier ios dev. All rights reserved.
+//
+
+import Foundation
+
+
+class Song {
+    var title: String
+    var artist: String
+    
+    init(title: String, artist: String) {
+        self.title = title
+        self.artist = artist
+    }
+}
+
+
+
+
+
+
+
+
+
